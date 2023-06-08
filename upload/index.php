f73b23e55e1f31fbc0363c593bb3bbc201c674f0
marshallovski <68496774+marshallovski@users.noreply.github.com>
2023-06-08 11:49:46 +0200

    bug fixes and cheking for files

--- upload/index.php
+++ upload/index.php
@@ -4,6 +4,10 @@ $file = $_FILES['files'];
 $filePath   = '../uploads/' . basename($file['name']);
 $fileSizeMB = $file['size'] / 1024 / 1024;
 
+function prettylog($text) {
+    echo "<p style=\"font-family: sans-serif;\">{$text}</p>";
+}
+
 if (isset($file) && !is_null($file)) {
     // if file size is bigger than 128MB:
     if ($file['size'] > 128217728) {
@@ -15,7 +19,7 @@ if (isset($file) && !is_null($file)) {
         header('Location: /?message=fileExists');
     } else {
         move_uploaded_file($file['tmp_name'], $filePath);
-        header('Location: /?message=uploaded');
+        prettylog("Uploaded {$file['name']}! Download link: <a href=\"/uploads/{$file['name']}\" download=\"\">click here</a>");
         exit;
     }
-}
+} else prettylog('Please include `files` request param in POST query. One file per request.');
