4ed952905d62c3a987e615c1c0513db95b6e9e8b
marshallovski <68496774+marshallovski@users.noreply.github.com>
2023-06-30 23:22:07 +0200

    v1.1

--- upload/index.php
+++ upload/index.php
@@ -5,6 +5,7 @@ $filePath   = '../uploads/' . basename($file['name']);
 $fileSizeMB = $file['size'] / 1024 / 1024;
 
 function prettylog($text) {
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
     echo "<p style=\"font-family: sans-serif;\">{$text}</p>";
 }
 
@@ -14,6 +15,10 @@ if (isset($file) && !is_null($file)) {
         echo "Sorry, your file is too large. Max. file size: 128MiB. Your file size: {$fileSizeMB}MB";
         exit;
     }
+
+    // using legacy funtions for old PHP versions
+    if(strpos($filePath, '.php')) prettylog('PHP files are not allowed to upload!');
+    if(strpos($filePath, '.html')) prettylog('HTML files are not allowed to upload!');
 
     if (file_exists($filePath)) {
         header('Location: /?message=fileExists');
